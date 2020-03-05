c78fc46bedfde4ec16157e8066ed3c245313178c
Zhan Wu <alex.zhan.wu@icloud.com>
2020-03-04 22:39:20 -0800

    Updates

---
+++ precache-manifest.980ade4ea38150bcdb4b4789a97fa3a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cb93c358c48f9d140777fef7af70c9",
+    "url": "/project-wz0805-1736705/index.html"
+  },
+  {
+    "revision": "1ba2aee5a9f8b2cb777c",
+    "url": "/project-wz0805-1736705/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b66e139f1f44b06efd6d",
+    "url": "/project-wz0805-1736705/static/css/main.4fec5290.chunk.css"
+  },
+  {
+    "revision": "1ba2aee5a9f8b2cb777c",
+    "url": "/project-wz0805-1736705/static/js/2.72b4bc9d.chunk.js"
+  },
+  {
+    "revision": "03f571cd0c3e4eb469324779825a0883",
+    "url": "/project-wz0805-1736705/static/js/2.72b4bc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66e139f1f44b06efd6d",
+    "url": "/project-wz0805-1736705/static/js/main.9b06273e.chunk.js"
+  },
+  {
+    "revision": "f526e54ec658226d40bb",
+    "url": "/project-wz0805-1736705/static/js/runtime-main.2bb60a73.js"
+  },
+  {
+    "revision": "8c0edfea00d0c83450e65de50fecc70b",
+    "url": "/project-wz0805-1736705/static/media/background.8c0edfea.jpg"
+  },
+  {
+    "revision": "88b4fb9f8ba9382fb8cba1776873fedd",
+    "url": "/project-wz0805-1736705/static/media/file1.88b4fb9f.csv"
+  },
+  {
+    "revision": "f52d408483647bc06b0bfa8ea6e04b86",
+    "url": "/project-wz0805-1736705/static/media/pasta.f52d4084.jpg"
+  },
+  {
+    "revision": "2e60864cca5e270a5bd2142f0562ee8d",
+    "url": "/project-wz0805-1736705/static/media/saved.2e60864c.jpg"
+  }
+]);
